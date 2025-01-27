1293bc0c5e3bcb6ba9d2182c1ac3bc95094c7c11
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2025-01-28 09:30:52 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',      // Added for JavaScript files
-        './resources/**/*.vue'         // Added for Vue components (if applicable)
+        './resources/js/**/*.vue',
+        './resources/js/**/*.js',
     ],
 
     theme: {
