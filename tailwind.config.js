5b58aa4348b5d565fbecd64a53465966cfb1127b
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2025-01-29 11:17:39 +1100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
-
+ 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        './resources/js/**/*.js',
     ],
-
+ 
     theme: {
         extend: {
             fontFamily: {
@@ -18,6 +16,6 @@ export default {
             },
         },
     },
-
+ 
     plugins: [forms],
 };
